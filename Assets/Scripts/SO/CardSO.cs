465dfeac59bb5347792af9017ddcdd33c6310934
Cheshire75 <jhr75468@gmail.com>
2024-08-22 10:45:27 +0900

    TextMeshPro, DoTween, 군대 추가(돌격대 제외)

---
+++ Assets/Scripts/SO/CardSO.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Card
+{
+    public string name;
+    public string attribute;
+    public int point;
+    public string effect;
+    public GameObject card_prefab;
+}
+
+[CreateAssetMenu(fileName = "CardSO", menuName = "Scriptable Object/CardSO")]
+public class CardSO : ScriptableObject
+{
+    public Card[] cards;
+}
