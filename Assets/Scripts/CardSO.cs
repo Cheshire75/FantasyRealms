76926798908bd604d5721e37edad2706ed242892
Cheshire75 <jhr75468@gmail.com>
2024-10-07 10:39:44 +0900

    Update CardSO.cs

--- Assets/Scripts/CardSO.cs
+++ Assets/Scripts/CardSO.cs
@@ -1,10 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+
+[System.Serializable]
+public class Item
+{
+    public string cardName;
+    public string attrib;
+    public int power;
+    public int order;
+    public bool isAvailable;
+    public bool isPenalty;
+    public GameObject prefab;
+    public Hand hand;
+}
 
 [CreateAssetMenu(fileName = "CardSO", menuName = "ScriptableObject/CardSO")]
 public class CardSO : ScriptableObject
 {
     [SerializeField]
     public Card[] AllCards = new Card[55];
+
 }
